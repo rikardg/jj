f0ed691c135c4ecee666331d3f9d2914e889bd6a
Gustavo Inacio <incio.gusta@gmail.com>
2026-05-02 16:11:59 +0200

    git: fix LFS performance and auto-enable config on track/untrack
    - Default git.lfs to false (opt-in) to avoid overhead in non-LFS repos
    - track auto-sets git.lfs=true in repo config
    - untrack removes git.lfs when no LFS patterns remain
    - Skip gitattributes filter_matches when ignore_filters is empty
    - Use globset for pattern matching in track/untrack touch logic
    - Touch affected files on untrack to force re-snapshot as regular content

--- lib/src/local_working_copy.rs
+++ lib/src/local_working_copy.rs
@@ -1705,10 +1705,11 @@ impl FileSnapshotter<'_> {
             if let Some(progress) = self.progress {
                 progress(&path);
             }
-            if self
-                .git_attributes
-                .filter_matches(&path, &self.ignore_filters, SearchPriority::Disk)
-                .block_on()
+            if !self.ignore_filters.is_empty()
+                && self
+                    .git_attributes
+                    .filter_matches(&path, &self.ignore_filters, SearchPriority::Disk)
+                    .block_on()
             {
                 // Skip gitattributes files that we want to ignore - this
                 // would result in them showing up as deleted, but we also
@@ -1859,10 +1860,11 @@ impl FileSnapshotter<'_> {
             .filter(|(_, state)| state.file_type != FileType::GitSubmodule)
             // Whether or not the entry exists, ignored gitattributes files should be omitted
             .filter(|(path, _)| {
-                !self
-                    .git_attributes
-                    .filter_matches(path, &self.ignore_filters, SearchPriority::Disk)
-                    .block_on()
+                self.ignore_filters.is_empty()
+                    || !self
+                        .git_attributes
+                        .filter_matches(path, &self.ignore_filters, SearchPriority::Disk)
+                        .block_on()
             })
             .filter(|(path, _)| self.matcher.matches(path))
             .try_for_each(|(path, _)| self.deleted_files_tx.send(path.to_owned()))
